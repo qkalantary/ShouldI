7aac4f89578661e01e6dc82c32993cf8ac6f7a48
Q Kalantary <qkalantary@gmail.com>
2014-12-06 21:05:33 -0500

    UI started

--- ShouldI/HomeBase.swift
+++ ShouldI/HomeBase.swift
@@ -10,15 +10,47 @@ import UIKit
 
 class HomeBase: UITableViewController {
 
+    func uicolorFromHex(rgbValue:UInt32)->UIColor{
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:1.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+       
+        tableView.backgroundColor = uicolorFromHex(0xBF0404)//UIColor(red: 255/255.0, green: 255/255.0, blue: 117/255.0, alpha: 1.0)
+        tableView.separatorColor =  UIColor.blackColor()//UIColor(red: 0/255.0, green: 163/255.0, blue: 136/255.0, alpha: 1.0)
+
+        
+        if self.tableView.respondsToSelector("setSeparatorInset:") {
+            self.tableView.separatorInset = UIEdgeInsetsZero
+        }
+        if self.tableView.respondsToSelector("setLayoutMargins:") {
+            self.tableView.layoutMargins = UIEdgeInsetsZero
+        }
+        
+        self.tableView.layoutIfNeeded()
+
+            }
+    
+    override func viewDidAppear(animated: Bool) {
+      //  print(self.parentViewController?.view)
+     ///   self.tableView
+     //   self.parentViewController?.view.backgroundColor = UIColor.blueColor()//
+
     }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -44,7 +76,19 @@ class HomeBase: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("poll", forIndexPath: indexPath) as UITableViewCell
 
         // Configure the cell...
-
+        if(indexPath.row % 2 == 0){
+            //cell.backgroundColor = UIColor(red: 240/255.0, green: 199/255.0, blue: 85/255.0, alpha: 1.0)
+        } else{
+            //cell.backgroundColor = UIColor(red: 226/255.0, green: 173/255.0, blue: 59/255.0, alpha: 1.0)
+        }
+        
+        //cell.backgroundColor = UIColor.clearColor()//UIColor(red: 255/255.0, green: 255/255.0, blue: 117/255.0, alpha: 1.0)
+        if cell.respondsToSelector("setSeparatorInset:") {
+            cell.separatorInset = UIEdgeInsetsZero
+        }
+        if cell.respondsToSelector("setLayoutMargins:") {
+            cell.layoutMargins = UIEdgeInsetsZero
+        }
         return cell
     }
     
